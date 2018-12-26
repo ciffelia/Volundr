e5b0abb00da78ab23259ef2c314cb2149001cb58
Ciffelia <mc.prince.0203@gmail.com>
2018-12-26 22:42:11 +0900

    不要なActivityを非表示に変更

--- app/src/main/java/com/ciffelia/volundr/MainActivity.kt
+++ app/src/main/java/com/ciffelia/volundr/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.ciffelia.volundr
 
 import android.app.Activity
-import android.app.PendingIntent
 import android.content.Intent
 import android.os.Bundle
 
@@ -9,7 +8,7 @@ class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        finish()
 
         val serviceIntent = Intent(this, ForegroundService::class.java)
         startForegroundService(serviceIntent)
