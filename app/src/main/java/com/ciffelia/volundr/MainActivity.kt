ba4c2d93b96d1ee4ca4794ddf80dc8cb8462660b
Ciffelia <mc.prince.0203@gmail.com>
2019-01-04 23:24:02 +0900

    Update MainActivity.kt

--- app/src/main/java/com/ciffelia/volundr/MainActivity.kt
+++ app/src/main/java/com/ciffelia/volundr/MainActivity.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 
+
 class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
